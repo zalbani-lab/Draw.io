16ca7aa0f786a1be3523e160cb66558e2d5f86f8
Alban <pro.pierson.alban@gmail.com>
2020-11-04 16:12:36 +0100

    Add some style

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("hello from server !!!")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () =>{
+    console.log("server running on port " + PORT)
+});
+app.use(express.static("public"));
